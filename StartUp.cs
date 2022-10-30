93d487536605fb18672a73ccb3a716fd6a411a1a
ZubaeyrMSFT <zmohammed@microsoft.com>
2022-10-31 02:45:32 +0530

    startup file

--- StartUp.cs
+++ StartUp.cs
@@ -10,14 +10,8 @@ namespace WordPressMigrationTool
         {
             //// To customize application configuration such as set high DPI settings or default font,
             //// see https://aka.ms/applicationconfiguration.
-            //ApplicationConfiguration.Initialize();
-            //Application.Run(new MigrationUX());
-
-            SiteInfo siteInfo = new SiteInfo();
-            siteInfo.subscriptionId = "b57e15e1-5109-4368-b4e1-ad5230344ad8";
-            siteInfo.resourceGroupName = "wpwinmig1-rg";
-            siteInfo.webAppName = "wpwinmig1-app";
-            Console.WriteLine(new ExportService().ExportDataFromSourceSite(siteInfo));
+            ApplicationConfiguration.Initialize();
+            Application.Run(new MigrationUX());
         }
     }
 }
